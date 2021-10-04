51965c8b10f5c6eedd62dd6b94b1ba8b4bfbc3ca
Projecthing <admirethemakeroftime@gmail.com>
2021-10-04 13:48:49 +0300

    Handle viewing nonexistent board and work on "Rule" viewing shader

--- Controller.cs
+++ Controller.cs
@@ -11,10 +11,9 @@ public class Controller : MonoBehaviour
     public int generations = 32;
     public byte rule = 30;
     public int boardToView = 0;
-    public int viewMode = 0;
     public int flipTimeDim = 1;
+    public string viewMode = "Vanilla";
 
-    private int kernelID;
     private int lastboardViewed = -1;
     private List<byte[]> initData;
     private Dispatcher dispatcher;
@@ -22,8 +21,11 @@ public class Controller : MonoBehaviour
     private System.Threading.Thread dispatcherThread;
     private Texture2D dataBuffer;
     private RenderTexture canvas;
+    private ComputeBuffer colorBuffer;
 
-    private float[] ruleColors = new float[8 * 4]{
+    private Dictionary<string,int> kernelIDs;
+
+    private float[] ruleColors = new float[]{
         0.0f, 0.0f, 0.0f, 1.0f,
         1.0f, 0.0f, 0.0f, 1.0f,
         1.0f, 1.0f, 0.0f, 1.0f,
@@ -36,6 +38,10 @@ public class Controller : MonoBehaviour
 
     void Start()
     {
+        this.kernelIDs = new Dictionary<string, int>(){
+            {"Vanilla", this.viewportShader.FindKernel("Vanilla")},
+            {"Rule", this.viewportShader.FindKernel("Rule")},
+        };
         //this.initData = GenerateRandom(width, 16);
         this.initData = new List<byte[]>();
         this.initData.Add(new byte[2]{0x00, 0x80});
@@ -48,31 +54,46 @@ public class Controller : MonoBehaviour
         this.canvas = new RenderTexture(width, generations, 24);
         this.canvas.enableRandomWrite = true;
         this.canvas.filterMode = FilterMode.Point;
-        this.kernelID = this.viewportShader.FindKernel("Viewport");
-        this.viewportShader.SetFloats("ruleColors", ruleColors);
-        this.viewportShader.SetInts("dims", new int[2]{width, generations});
-        this.viewportShader.SetTexture(kernelID, "input", this.dataBuffer);
-        this.viewportShader.SetTexture(kernelID, "output", this.canvas);
+        this.colorBuffer = new ComputeBuffer(8, 16);
+        this.colorBuffer.SetData(ruleColors);
     }
 
     void OnRenderImage(RenderTexture src, RenderTexture dest){
         bool dispatchShader = true;
         if(lastboardViewed != boardToView){
-            if(this.data.boards[boardToView].isSimulated){
-                this.dataBuffer.SetPixelData<byte>(this.data.boards[boardToView].ToByteStream(sizeof(int)), 0, 0);
-                this.dataBuffer.Apply();
-                lastboardViewed = boardToView;
+            try{
+                if(this.data.boards[boardToView].isSimulated){
+                    //var forChecking = this.data.boards[boardToView].ToByteStream(sizeof(int));
+                    //foreach(var t in forChecking){Debug.Log(t.ToString());}
+                    this.dataBuffer.SetPixelData(this.data.boards[boardToView].ToByteStream(sizeof(int)), 0, 0);
+                    this.dataBuffer.Apply();
+                    lastboardViewed = boardToView;
+                }
+                else {
+                    dispatchShader = false;
+                    Debug.Log("Requested board is not yet simulated!");
+                }
             }
-            else {
+            catch (System.IndexOutOfRangeException a){
                 dispatchShader = false;
-                Debug.Log("Requested board does not exist or has not yet been simulated!");
+                boardToView = 0;
+                Debug.Log("Requested board does not exist!");
             }
+            
         }
         if(dispatchShader){
-            this.viewportShader.SetInts("viewMode", new int[2]{viewMode, flipTimeDim});
-            this.viewportShader.Dispatch(kernelID, width / SHADERTHREADGROUPS, generations / SHADERTHREADGROUPS, 1);
+            this.viewportShader.SetInts("dims", new int[2]{width, generations});
+            this.viewportShader.SetInt("flipVertical", flipTimeDim);
+            this.viewportShader.SetBuffer(this.kernelIDs[viewMode], "ruleColors", colorBuffer);
+            this.viewportShader.SetTexture(this.kernelIDs[viewMode], "input", this.dataBuffer);
+            this.viewportShader.SetTexture(this.kernelIDs[viewMode], "output", this.canvas);
+            this.viewportShader.Dispatch(this.kernelIDs[viewMode], width / SHADERTHREADGROUPS, generations / SHADERTHREADGROUPS, 1);
         }
         Graphics.Blit(this.canvas, dest);
+    }
+
+    void OnApplicationquit(){
+        this.colorBuffer.Release();
     }
 
     private List<byte[]> GenerateRandom(int width, int boards){
