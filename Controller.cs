7e6dc2ae0f748f813a7dd9eacaa9fd0e597e22de
Projecthing <admirethemakeroftime@gmail.com>
2021-10-05 21:29:47 +0300

    Query Engine (sort of working)

--- Controller.cs
+++ Controller.cs
@@ -8,10 +8,13 @@ public class Controller : MonoBehaviour
     private static readonly int SHADERTHREADGROUPS = 8;
 
     public ComputeShader viewportShader;
+    public ComputeShader queryShader;
     public int boardToView = 0;
     public int simulationToView = 0;
     public int flipTimeDim = 1;
     public string viewMode = "Vanilla";
+    public QueryEngine.QueryType queryType = QueryEngine.QueryType.State;
+    public bool viewQueries = false;
     public int width = 32;
     public int generations = 32;
     public byte[] rules = {30};
@@ -20,7 +23,10 @@ public class Controller : MonoBehaviour
     private (int board, int sim) boardSimToView = (0, 0);
     private (int board, int sim) lastBoardSimViewed = (-1, -1);
     private List<SimulationData> data;
+    private List<List<QueryEngine.QueryBoard>> queries;
+    private QueryEngine queryEngine;
     private Thread simulationThread;
+    private Thread queryThread;
     private RenderTexture canvas;
     private ComputeBuffer dataBuffer;
     private ComputeBuffer colorBuffer;
@@ -45,16 +51,21 @@ public class Controller : MonoBehaviour
         this.kernelIDs = new Dictionary<string, int>(){
             {"Vanilla", this.viewportShader.FindKernel("Vanilla")},
             {"Rule", this.viewportShader.FindKernel("Rule")},
-            {"Test", this.viewportShader.FindKernel("Test")},
         };
         this.initData = new List<byte[]>();
         this.initData.Add(new byte[4]{0x00, 0x80, 0x80, 0x00});
         this.colorBuffer = new ComputeBuffer(8, 4 * sizeof(float));
         this.RunSimulation();
+        this.queries = new List<List<QueryEngine.QueryBoard>>();
+        this.queryEngine = new QueryEngine(this.data, QueryEngine.QueryType.State);
         this.simulationThread.Join();
+        this.queryEngine.RunQuery();
     }
 
     void Update(){
+        if(this.queryEngine.GetStatus(true) == QueryEngine.QueryStatus.Finished){
+            this.queries.Add(this.queryEngine.result);
+        }
         if(Input.GetKeyDown(KeyCode.V)){
             this.ReSetViewportParameters();
         }
@@ -67,9 +78,26 @@ public class Controller : MonoBehaviour
             this.lastBoardSimViewed = (-1, -1);
         }
         if(Input.GetKeyDown(KeyCode.U)){
-            //user code stuff todo
-            this.initData = GenerateCount(width, 16);
-            foreach(var t in this.initData[3]){Debug.Log(t.ToString());}
+            try{
+                //user code stuff todo
+                this.rules = SetRuleRange(0, 127);
+                this.initData = GenerateCount(width, 16);
+                //this.initData = GenerateRandom(16, 2048);
+            }
+            catch {
+                Debug.Log("Entered script not valid!");
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Q)){
+            if(this.simulationThread.IsAlive){
+                Debug.Log("Simulation not yet done! Can't query yet!");
+            }
+            else {
+                Debug.Log("Query started at " + System.DateTime.Now.ToString());
+                this.queryEngine = new QueryEngine(this.data, queryType);
+                this.queryThread = new Thread(this.queryEngine.RunQuery);
+                this.queryThread.Start();
+            }
         }
     }
 
@@ -84,6 +112,7 @@ public class Controller : MonoBehaviour
     void RunSimulation(){
         boardToView = 0;
         simulationToView = 0;
+        viewQueries = false;
         this.data = new List<SimulationData>();
         this.simulationThread = new Thread(this.RunSimulationThread);
         this.simulationThread.Start();
@@ -115,29 +144,75 @@ public class Controller : MonoBehaviour
     void OnRenderImage(RenderTexture src, RenderTexture dest){
         bool dispatchShader = true;
         if(this.lastBoardSimViewed != boardSimToView){
-            try{
-                lock(this.data[boardSimToView.sim].boards[boardSimToView.board]){
-                    this.dataBuffer.SetData(this.data[boardSimToView.sim].boards[boardSimToView.board].ToByteStream(sizeof(int)));
+            if(viewQueries){
+                try{
+                    var board = this.queries[boardSimToView.sim][boardSimToView.board].board;
+                    if(this.dataBuffer.count != board.Length){
+                        this.dataBuffer.Release();
+                        this.dataBuffer = new ComputeBuffer(board.Length, sizeof(int));
+                    }
+                    this.dataBuffer.SetData(this.queries[boardSimToView.sim][boardSimToView.board].board);
+                    Debug.Log(this.queries[boardSimToView.sim][boardSimToView.board].descriptor);
+                    lastBoardSimViewed = boardSimToView;
                 }
-                lastBoardSimViewed = boardSimToView;
+                catch (System.ArgumentOutOfRangeException){
+                    dispatchShader = false;
+                    boardSimToView = (0, 0);
+                    Debug.Log("Requested query does not exist!");
+                }
             }
-            catch(System.IndexOutOfRangeException){
-                dispatchShader = false;
-                boardSimToView = this.lastBoardSimViewed;
-                Debug.Log("Requested board does not exist! Maybe not simulated yet or wrong index?");
+            else {
+                try{
+                    if(this.dataBuffer.count != this.data[0].width * this.data[0].generations){
+                        this.dataBuffer.Release();
+                        this.dataBuffer = new ComputeBuffer(this.data[0].width * this.data[1].generations, sizeof(int));
+                    }
+                    lock(this.data[boardSimToView.sim].boards[boardSimToView.board]){
+                        this.dataBuffer.SetData(this.data[boardSimToView.sim].boards[boardSimToView.board].ToByteStream(sizeof(int)));
+                    }
+                    lastBoardSimViewed = boardSimToView;
+                }
+                catch(System.ArgumentOutOfRangeException){
+                    dispatchShader = false;
+                    boardSimToView = (0, 0);
+                    Debug.Log("Requested board does not exist! Maybe not simulated yet or wrong index?");
+                }
             }
         }
         if(dispatchShader){
-            (int w, int h) dims;
-            lock(this.data[boardSimToView.sim]){
-                dims = (this.data[boardSimToView.sim].width, this.data[boardSimToView.sim].generations);
+            if(viewQueries){
+                var board = this.queries[boardSimToView.sim][boardSimToView.board];
+                if(this.canvas.width != board.dims.width || this.canvas.height != board.dims.generations){
+                    this.canvas.Release();
+                    this.canvas = new RenderTexture(board.dims.width, board.dims.generations, 24);
+                    this.canvas.enableRandomWrite = true;
+                    this.canvas.filterMode = FilterMode.Point;
+                }
+                this.queryShader.SetInt("flipVertical", this.timeDimDirection);
+                this.queryShader.SetInts("dims", new int[2]{board.dims.width, board.dims.generations});
+                this.queryShader.SetInts("MaxMin", new int[2]{board.valueRange.max, board.valueRange.min});
+                this.queryShader.SetBuffer(0, "input", this.dataBuffer);
+                this.queryShader.SetTexture(0, "output", this.canvas);
+                this.queryShader.Dispatch(0, board.dims.width / SHADERTHREADGROUPS, board.dims.generations / SHADERTHREADGROUPS, 1);
             }
-            this.viewportShader.SetInt("flipVertical", this.timeDimDirection);
-            this.viewportShader.SetInts("dims", new int[2]{dims.w, dims.h});
-            this.viewportShader.SetBuffer(this.kernelIDs[this.shaderToUse], "ruleColors", this.colorBuffer);
-            this.viewportShader.SetBuffer(this.kernelIDs[this.shaderToUse], "input", this.dataBuffer);
-            this.viewportShader.SetTexture(this.kernelIDs[this.shaderToUse], "output", this.canvas);
-            this.viewportShader.Dispatch(this.kernelIDs[this.shaderToUse], dims.w / SHADERTHREADGROUPS, dims.h / SHADERTHREADGROUPS, 1);
+            else {
+                (int w, int h) dims;
+                lock(this.data[boardSimToView.sim]){
+                    dims = (this.data[boardSimToView.sim].width, this.data[boardSimToView.sim].generations);
+                }
+                if(this.canvas.width != dims.w || this.canvas.height != dims.h){
+                    this.canvas.Release();
+                    this.canvas = new RenderTexture(dims.w, dims.h, 24);
+                    this.canvas.enableRandomWrite = true;
+                    this.canvas.filterMode = FilterMode.Point;
+                }
+                this.viewportShader.SetInt("flipVertical", this.timeDimDirection);
+                this.viewportShader.SetInts("dims", new int[2]{dims.w, dims.h});
+                this.viewportShader.SetBuffer(this.kernelIDs[this.shaderToUse], "ruleColors", this.colorBuffer);
+                this.viewportShader.SetBuffer(this.kernelIDs[this.shaderToUse], "input", this.dataBuffer);
+                this.viewportShader.SetTexture(this.kernelIDs[this.shaderToUse], "output", this.canvas);
+                this.viewportShader.Dispatch(this.kernelIDs[this.shaderToUse], dims.w / SHADERTHREADGROUPS, dims.h / SHADERTHREADGROUPS, 1);
+            }
         }
         Graphics.Blit(this.canvas, dest);
     }
--- QueryEngine.cs
+++ QueryEngine.cs
@@ -1,118 +1,149 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
 
-public class QueryResult{
+public class QueryEngine{
+    public enum QueryType{
+        State, Rule, User,
+    };
+
+    public enum QueryStatus{
+        Finished, Running, Waiting, Ended,
+    }
+
     public struct QueryBoard{
         public int[] board;
         public string descriptor;
-        public QueryBoard(int width, int generations, string descriptor){
-            this.board = new int[width * generations];
-            this.descriptor = descriptor;
+        public (int max, int min) valueRange;
+        public (int width, int generations) dims;
+        public QueryBoard(int[] b, string desc, (int, int) valRange, (int, int) dims){
+            this.board = b;
+            this.descriptor = desc;
+            this.valueRange = valRange;
+            this.dims = dims;
         }
-    }
+    };
 
-    public QueryBoard[] boards;
-    public QueryEngine.QueryType queryType;
-    public int width;
-    public int generations;
-    public string rules;
-    public QueryResult(QueryEngine.QueryType type, int width, int generations, string ruleString){
-        switch(type){
-            case QueryEngine.QueryType.State:   this.boards = new QueryBoard[2]; break;
-            case QueryEngine.QueryType.Rule:    this.boards = new QueryBoard[8]; break;
-            case QueryEngine.QueryType.Both:    this.boards = new QueryBoard[10]; break;
-        }
-        this.width = width;
-        this.generations = generations;
+    public QueryType queryType;
+    public List<QueryBoard> result;
+    private int width;
+    private int generations;
+    private List<SimulationData> data;
+    public QueryStatus status;
+
+    public QueryEngine(List<SimulationData> dataIn, QueryType type){
         this.queryType = type;
-        this.rules = ruleString;
+        this.status = QueryStatus.Waiting;
+        this.data = dataIn;
+        this.width = this.data[0].width;
+        this.generations = this.data[0].generations;
+        this.result = new List<QueryBoard>();
     }
 
-    public void SaveToFile(){
+    public void RunQuery(){
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+        timer.Start();
+        lock(this){
+            this.status = QueryStatus.Running;
+        }
+        switch(this.queryType){
+            case QueryType.State:   this.QueryState(); break;
+            case QueryType.Rule:    this.QueryRule(); break;
+            case QueryType.User:    this.QueryUser(); break;
+            default:                this.QueryFail(); break;
+        }
+        foreach(var n in this.result[0].board){Debug.Log(n.ToString());}
+        lock(this){
+            this.status = QueryStatus.Finished;
+        }
+        timer.Stop();
+        Debug.Log("Query completed in " + timer.Elapsed.ToString());
+    }
+
+    private void QueryState(){
+        int boardSize = this.width * this.generations;
+        int[] aliveCounts = new int[boardSize];
+        int[] deadCounts = new int[boardSize];
+        for(int i = 0; i < boardSize; i++){
+            aliveCounts[i] = 0;
+            deadCounts[i] = 0;
+        }
+        foreach(var simulation in data){
+            foreach(var board in simulation.boards){
+                for(int i = 0; i < this.generations; i++){
+                    var offset = i * this.width;
+                    for(int j = 0; j < this.width; j++){
+                        if(board.board[i][j].state == 0){ deadCounts[offset + j]++; }
+                        else { aliveCounts[offset + j]++; }
+                    }
+                }
+            }
+        }
+        this.result.Add(new QueryBoard(aliveCounts, "Alive Count", FindMaxMin(aliveCounts), (this.width, this.generations)));
+        this.result.Add(new QueryBoard(deadCounts, "Dead Count", FindMaxMin(deadCounts), (this.width, this.generations)));
+    }
+
+    private void QueryRule(){
+        int boardSize = this.width * this.generations;
+        List<int[]> ruleCounts = new List<int[]>(8);
+        for(int i = 0; i < 8; i++){
+            ruleCounts.Add(new int[boardSize]);
+            for(int j = 0; j < boardSize; j++){
+                ruleCounts[i][j] = 0;
+            }
+        }
+        foreach(var simulation in data){
+            foreach(var board in simulation.boards){
+                for(int i = 0; i < this.generations; i++){
+                    var offset = i * this.width;
+                    for(int j = 0; j < this.width; j++){
+                        ruleCounts[board.board[i][j].rule][offset + j]++;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < 8; i++){
+            this.result.Add(new QueryBoard(ruleCounts[i], ("Count for " + IntToBinString(i, 3)), FindMaxMin(ruleCounts[i]), (this.width, this.generations)));
+        }
+    }
+
+    private void QueryUser(){
         //todo
+        this.QueryFail();
     }
 
-    public static string MakeRuleString(byte[] rules){
-        string output = rules[0].ToString();
-        int counter = (int)rules[0];
-        byte lastRule = rules[0];
-        foreach(byte rule in rules){
-            //todo
+    private void QueryFail(){
+        int boardSize = this.width * this.generations;
+        int[] emptyBoard = new int[boardSize];
+        for(int i = 0; i < boardSize; i++){ emptyBoard[i] = 0; }
+        this.result.Add(new QueryBoard(emptyBoard, "INVALID", (0, 0), (this.width, this.generations)));
+    }
+
+    public QueryStatus GetStatus(bool markEndedWhenFinished){
+        var current = this.status;
+        if(markEndedWhenFinished && (this.status == QueryStatus.Finished)) { this.status = QueryStatus.Ended; }
+        return current;
+    }
+
+    private (int, int) FindMaxMin(int[] arr){
+        var max = arr[0];
+        var min = arr[0];
+        for(int i = 0; i < arr.Length; i++){
+            max = arr[i] > max ? arr[i] : max;
+            min = arr[i] < min ? arr[i] : min;
+        }
+        return (max, min);
+    }
+
+    private string IntToBinString(int number, byte bits){
+        string output = "";
+        var b = bits > 32 ? 32 : bits;
+        for(int i = 0; i < b; i++){
+            bool bit = ((number >> i) & 0x1) > 0;
+            if(bit){ output = "1" + output; }
+            else {output = "0" + output; }
         }
         return output;
     }
 }
 
-public class QueryEngine
-{
-    public enum QueryType{
-        State, Rule, Both, User,
-    };
-
-    public QueryResult result;
-    public QueryType queryType;
-    public string userQueryFunction;
-    private List<SimulationData> data;
-    private int dataWidth;
-    private int dataGenerations;
-
-    public QueryEngine(){
-        this.queryType = QueryType.State;
-        var tempData = new List<SimulationData>(){new SimulationData()};
-        this.SetData(tempData);
-    }
-
-    public void SetData(List<SimulationData> newData){
-        this.data = newData;
-        this.dataWidth = this.data[0].width;
-        this.dataGenerations = this.data[0].generations;
-    }
-
-    public void DispatchQuery(){
-        Debug.Log("Query started at " + System.DateTime.Now.ToString());
-        switch(this.queryType){
-            case QueryType.State:   this.QueryState(); break;
-            case QueryType.Rule:    this.QueryRule(); break;
-            case QueryType.Both:    this.QueryBoth(); break;
-            case QueryType.User:    this.QueryUser(); break;
-            default:                this.NoQuery(); break;
-        }
-    }
-
-    public void QueryState(){
-        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
-        timer.Start();
-        
-        timer.Stop();
-        Debug.Log("Query completed in " + timer.Elapsed.ToString());
-    }
-    public void QueryRule(){
-        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
-        timer.Start();
-        
-        timer.Stop();
-        Debug.Log("Query completed in " + timer.Elapsed.ToString());
-    }
-    public void QueryBoth(){
-        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
-        timer.Start();
-        
-        timer.Stop();
-        Debug.Log("Query completed in " + timer.Elapsed.ToString());
-    }
-    public void QueryUser(){
-        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
-        timer.Start();
-        
-        timer.Stop();
-        Debug.Log("Query completed in " + timer.Elapsed.ToString());
-    }
-    public void NoQuery(){
-        QueryResult output = new QueryResult(QueryType.State, 1, 1, "Invalid query");
-        this.result = 
-        this.result.boards
-        Debug.Log("Invalid query type! Output is invalid.");
-    }
-}
